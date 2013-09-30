52b483fa81dcc05126cb5f4334fc47c282528803
Ubuntu <ubuntu@ip-10-29-145-135.ec2.internal>
2013-09-30 21:29:23 +0000

    feat(S3 Benchmarking): Graph and script added

---
+++ S3_test.py
@@ -0,0 +1,62 @@
+import boto
+from boto.s3.key import Key
+import time
+import os
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+def S3Connection():
+    connection = boto.connect_s3()
+    return connection
+
+def S3checker(connection,filename):
+    start=time.time()
+    bucket = connection.get_bucket('srirams3bucket')  
+    key=Key(bucket)
+    key.key=filename
+    key.set_contents_from_filename(filename)
+    time_taken = time.time()-start
+    print 'It took ', time_taken, ' seconds'
+    return time_taken
+
+def filesizegenerator():
+    return [x*1024*1024*5 for x in xrange(1,50)]
+
+def filenamegenerator():
+    return ['test'+str(i) for i in xrange(1,50)]
+
+def create_file(path, size, generator=os.urandom):
+    """Creates a file @ `path` of size in bytes `size` using
+    a generator function `generator`. Generator must take a single
+    argument, a size in bytes. By default, the generator is os.urandom.
+    """
+    with open(path, 'wb') as f:
+        f.write(generator(size))
+
+def filecreator():
+    filenames= filenamegenerator()
+    filesizes= filesizegenerator()
+    actual_file_sizes = [filesize/(1024*1024) for filesize in filesizes]
+    for filename,filesize in zip(filenames,filesizes):
+        create_file(os.path.join(os.getcwd(),filename),filesize)
+
+def main():
+    connection = S3Connection()
+    time_taken_series = []
+    filecreator()
+    filesizes= filesizegenerator()
+    actual_file_sizes = [filesize/(1024*1024) for filesize in filesizes]
+    filenames = filenamegenerator()
+    for filename in filenames:
+        time_taken_series.append(S3checker(connection,filename))
+    plt.plot(actual_file_sizes, time_taken_series,label="File Sizes (in MB)  vs Time taken to upload to S3 (in seconds)")
+    plt.xlabel("File Sizes in MB")
+    plt.ylabel("Time taken in seconds")
+    plt.title("Upload rate difference with file size")
+    plt.xlim(5, 250)
+    plt.ylim(0, 50)
+    plt.legend(loc="upper right")
+    plt.savefig("S3_performance.png")
+if __name__ == '__main__':
+    main()
